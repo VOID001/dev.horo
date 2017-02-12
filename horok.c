bfb05760023145700c4a0611e4efa1ae324439f8
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2017-02-12 12:58:39 +0800

    Cleanup Code

--- horok.c
+++ horok.c
@@ -7,28 +7,20 @@
 #include <linux/netpoll.h>
 
 #define M_MISC_MAJOR 0
-#define FEED "feed"
-#define WASH "wash"
-#define EIDLEN 13
-
-struct horo_data {
-	int clean;
-	int hunger
-};
-
-struct horo_data Horo;
 
 static struct miscdevice misc;
 
-char kbuf[4096];
+char kbuf[PAGE_SIZE];
 
+// horo_read displays info to user stored by horo_ioctl
 static ssize_t horo_read(struct file *filp, char __user *buf,
 		size_t count, loff_t *offp)
 {
 	return simple_read_from_buffer(buf, count, offp, kbuf, strlen(kbuf) + 1);
 }
 
-static int call_horoproxy(const char *cmd) {
+// call_horoproxy calls usermode helper: horoproxy to do real tasks
+static int call_horoproxy(char *cmd) {
 	struct subprocess_info *sub_info;
 	char *argv[] = {"/usr/local/bin/horoproxy", cmd, NULL};
 	static char *envp[] = {
@@ -36,24 +28,27 @@ static int call_horoproxy(const char *cmd) {
 		"TERM=linux",
 		NULL
 	};
+
 	sub_info = call_usermodehelper_setup(argv[0], argv, envp, GFP_ATOMIC, NULL, NULL, NULL);
 	if (sub_info == NULL) return -ENOMEM;
 	return call_usermodehelper_exec(sub_info, UMH_WAIT_PROC);
 }
 
+// horo_write gets the command from user, and send it to horoproxy
 static ssize_t horo_write(struct file *filp, const char __user *buf,
 		size_t count, loff_t *offp)
 {
-	char kbuf[20] = "";
+	char *cmdbuf;
 	int ret;
 
+	cmdbuf = (char *)kmalloc(PAGE_SIZE, GFP_KERNEL);
 	memset(kbuf, 0, sizeof(kbuf));
-	pr_info("MARK");
 	ret = simple_write_to_buffer(kbuf, sizeof(kbuf), offp, buf, count);
 	if (ret < 0)
 		return ret;
 	pr_info("Kbuf: %s", kbuf);
 	ret = call_horoproxy(kbuf);
+	kfree(cmdbuf);
 	if(ret == 0)
 		return count;
 	return ret;
@@ -87,8 +82,6 @@ int horo_init(void)
 	misc.name = "horo";
 	misc.fops = &misc_fops;
 	ret = misc_register(&misc);
-	Horo.hunger = 100;
-	Horo.clean = 0;
 	if (ret < 0) {
 		pr_err("Cannot register misc horo\n");
 		return ret;
@@ -106,5 +99,5 @@ void horo_exit(void)
 module_init(horo_init);
 module_exit(horo_exit)
 
-MODULE_LICENSE("GPL v2");
-MODULE_AUTHOR("VOID001<tg @VOID001>");
+MODULE_LICENSE("GPL v3");
+MODULE_AUTHOR("VOID001<telegram @VOID001>");
